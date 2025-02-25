e3f332fd7d42a73f8e1e95bd7cfab0b4ff04c7d5
adityadkr007 <adityadkr1@gmail.com>
2025-02-25 17:23:01 +0530

    .

--- src/app/features/public/landing/landing.component.ts
+++ src/app/features/public/landing/landing.component.ts
@@ -4,8 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-landing',
   standalone: false,
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrl: './landing.component.css',
 })
 export class LandingComponent {
-  progress = 10
+  progress = 10;
+
+  ngOnInit() {
+    const navBtn = document.getElementById('menu-btn');
+    const navList = document.querySelector('.nav-list');
+    navBtn?.addEventListener('click', () => {
+      navList?.classList.toggle('active');
+    });
+  }
+  scrollToSection(name: string) {
+    document.getElementById(name)?.scrollIntoView({ behavior: 'smooth' });
+  }
 }
